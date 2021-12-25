eb3ad01f51469f4caccd32b02e2d3fa3d08ee2dc
osakana373 <80421190+0sakana373@users.noreply.github.com>
2021-12-24 19:32:06 -0700

    DataCollecter object was made

--- src/DataCollector.py
+++ src/DataCollector.py
@@ -14,3 +14,38 @@ class SampleClass:
 
     def send_request(self, summoner_name):
         pass
+
+
+class DataCollector:
+    """
+    This collects data
+    """
+    def __init__(self):
+        self.summoner_name = None
+        self.summoner_id = None
+        self.summoner_puuid = None
+
+    def process_summoner_v4(self, json):
+        self.summoner_name = json["name"]
+        self.summoner_id = json["id"]
+        self.summoner_puuid = json["puuid"]
+
+"""
+Examples
+
+Given =
+{
+    "id": "lnLIOrFoH-2ZW3YHduv7TyeVUcJJkaIdNdSpdaIQZUBbX7U",
+    "accountId": "PZMNBb6Bj01iYWjnM_bkbe4W_Db2C_RS4Pn9ciPpMGLJKfk",
+    "puuid": "g0IivRIr4OJCLCB2tnuIm0F9_6W_7bb3ki5ON1-nJiIBs7Wy1uXXQ5ZDXjGIzPb3aBSiPTiAobdGfg",
+    "name": "PlatypusOfCanada",
+    "profileIconId": 744,
+    "revisionDate": 1640142865000,
+    "summonerLevel": 390
+}
+
+Take, Summoner name, id, puuid from summoner v4
+
+Andrewli = DataCollector() 
+Andrewli.process_summoner_v4(json)
+"""
