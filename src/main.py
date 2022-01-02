9e22d614bc92eef1a570cb609d54acfecc5cd3e3
dungwoong <78326933+dungwoong@users.noreply.github.com>
2022-01-02 12:42:24 -0700

    moved bad_summoners.csv

--- src/main.py
+++ src/main.py
@@ -13,7 +13,7 @@ import pandas as pd
 
 CSV_FILE = "../data/highelo/data.csv"
 GAME_FILE = "../data/highelo/games.csv"
-BAD_SUMMONER_FILE = "../planning/bad_summoners.csv"
+BAD_SUMMONER_FILE = "../data/bad_summoners.csv"
 COLUMNS = ['gameid', 'winning_team', '100_TOP_avg_game_time', '100_TOP_positions_played', '100_TOP_avg_kda',
            '100_TOP_avg_kp', '100_TOP_avg_vision', '100_TOP_avg_cs', '100_TOP_goldpm', '100_TOP_dmgpm',
            '100_TOP_dmg_to_obj_pm', '100_TOP_wr', '100_TOP_tier', '100_TOP_rank', '100_TOP_lp', '100_TOP_wins',
